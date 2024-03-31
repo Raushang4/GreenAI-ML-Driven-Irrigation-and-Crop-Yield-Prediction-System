b42ef3c14531c2a15e680dd026c381508faed9ca
Raushan Kumar <122561489+Raushang4@users.noreply.github.com>
2024-03-31 08:40:26 +0000

    first commit

---
+++ Model Development/main.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import warnings
+
+
+df1 = pd.read_csv("https://raw.githubusercontent.com/OmdenaAI/Algeria-Chapter-Green/main/Part%202/Subproject3-%20Recommendation%20Systems/data/Irrigation%20Dataset%20-%20NIT%20Raipur.csv", names = ["CropType", "CropDays", "SoilMoisture", "Temperature", "Humidity", "NeedIrrigation"])
+df2 = pd.read_excel("Irrigation Dataset - GCE Galgotias.xlsx")
+
+
+# Data Preprocessing
+df2 = df2.loc[:, ~df2.columns.str.contains('^Unnamed')]
+df2 = df2.drop(['Soil Temperature'], axis=1)
+df2.rename(columns = {'Irrigation(Y/N)':'NeedIrrigation', "Soil Moisture": "SoilMoisture", }, inplace = True)
+df2['CropType'] = df2['CropType'].replace(1,5)
+
+
+
+df = pd.concat([df1, df2])  # Merging the datasets
+df.drop(df[(df['Temperature'] > 100)].index, inplace=True)
+df = df.sort_values(by=['CropType'])
+
+
+print("--Preprocessed Data--")
+print(df.head())
+print(np.unique(df["CropType"]))
+
+# Data split: Features & Labels
+features = df[df.columns[:len(df.columns) - 1]]
+labels = df[df.columns[-1]]
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers = [("encoder", OneHotEncoder(), ["CropType"])], remainder = "passthrough")
+X = np.array(ct.fit_transform(features))
+y = np.array(labels)
+
+# Data split: Training & Testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Scaling the features
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+
+
+# Building the ANN
+modemodell = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(units = 6, activation = "relu"))
+model.add(tf.keras.layers.Dense(units = 6, activation = "relu"))
+model.add(tf.keras.layers.Dense(units = 1, activation = "sigmoid"))
+
+# Compiling the model
+model.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
+model.fit(X_train, y_train, epochs = 50)
+
+# Testing the model
+y_pred = model.predict(X_test)
+y_pred = (y_pred >model
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+print("Confusion Matrix")
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+print("Accuracy of the model:", accuracy_score(y_test, y_pred))
+
+# Save the model
+model.save("ANNmodel.h5")
---
+++ README.md
@@ -0,0 +1,39 @@
+![image](deployment/static/img/cover.png)<br/>
+![image](deployment/static/img/Screenshot(1757).png)
+
+
+# Description
+Smart irrigation: ML-Driven smart  Irrigation system 
+
+# Features
+Available and Upcoming:  
+✅ Recommendation System for Irrigation  
+✅ Artificial Neural Network Model  
+
+# Artificial Neural Network Architecture
+![Screenshot from 2024-03-09 00-16-52](https://user-images.githubusercontent.com/59119736/217623840-679a2bac-5639-4134-92d9-8e2c861d5194.png)
+
+
+
+ 
+# Tech Used
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) ![Flask](https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white) ![NumPy](https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white) ![Pandas](https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white) ![scikit-learn](https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white) ![Keras](https://img.shields.io/badge/Keras-%23D00000.svg?style=for-the-badge&logo=Keras&logoColor=white) ![TensorFlow](https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=for-the-badge&logo=TensorFlow&logoColor=white)
+      
+# Deployment Setup:
+### Installing dependencies:  
+`pip install -r requirements.txt`
+### Run the deployment:
+- Step 1: Change the present working directory  
+`cd deployment` 
+- Step 2: Run the `main.py` script  
+`python main.py`
+
+# Contributors
+
+<a href="https://github.com/raushang4/raushang4/graphs/contributors">
+  
+
+[Raushan kumar](https://github.com/raushang4/raushang4)  
+[contact](mailto:raushang4@gmail.com)
+
